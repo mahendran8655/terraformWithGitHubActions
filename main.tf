80e62277aa16a3f8ca201b337007539e18cc6b79
mahendran8655 <mahendran8655@gmail.com>
2024-11-26 19:50:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.78.0"
+    }
+  }
+}
 resource "google_compute_instance" "whizlabs" {
   name         = var.instance_name
   machine_type = var.instance_type
